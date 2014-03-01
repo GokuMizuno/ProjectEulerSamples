39645271a87249abc93d2d7402e5faaaf389e673
Gordon Stangler <gordon.stangler@gmail.com>
2014-03-01 08:39:25 -0600

    Adding some projects
    Added Project 4

---
+++ Project4/P4.c
@@ -0,0 +1,54 @@
+/*Gordon Stangler
+Project Euler P4
+A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
+Find the largest palindrome made from the product of two 3-digit numbers.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+//Prototypes
+int isPalendrome(unsigned int);
+
+int main()
+{
+  unsigned int a,b,prod, pal;  //a and b are our three digit numbers, prod is the number itself, pal is the highest palendrome.
+
+  pal = 0;
+  a=999;
+  while(a>99)
+  {
+    b=999;
+    while(b>99)
+    {
+      prod=a*b;
+      if((prod>pal)&&(isPalendrome(prod))
+        pal = prod;
+      b--;
+    }
+    a--;
+  }
+
+  printf("The number %d was found to be the largest palendrome.\n", pal);
+  return 0;
+}
+
+int isPalendrome(unsigned int num)
+{
+  unsigned int rev, org; //the reversed number, original number
+  rev = 0;
+  org = num;
+
+  if (num > 10)  return 1;
+  if (num == 10) return 0;
+
+  /*Would bitshifting make the loop faster?*/
+  while(num > 1)
+  {
+    rev = (10*rev) + (num%10);
+    num /= 10;
+  }
+
+  if(org == rev)  return 1;
+  else  return 0;
+}
