aaacb68a00d06bdf7b439ead8cd84171c56d0ff1
Gordon Stangler <gordon.stangler@gmail.com>
2014-04-26 20:08:26 -0500

    Editing P15
    Doing some cheap debugging

--- Project015/P15.cpp
+++ Project015/P15.cpp
@@ -1,13 +1,14 @@
 #include <iostream>
 
     const short gridsize = 3;
+	/*C arrays start at 0, not one, so for a gridsize of 3, the elements are 0,1,2.*/
 //not working, and I have no idea why.
 int main()
 {
    unsigned long grid[gridsize][gridsize];
-   unsigned long length;
+//   unsigned long length;
    short i,j;
-   i = j = 0;
+   i = j = 0; //needed?
    
    for(i=0;j<gridsize;i++)
    {
@@ -20,26 +21,38 @@ int main()
    }
    i^=i;
    j^=j;
+   std::cout << "i = " << i << " j = " << j << std::endl;
    
-   while(i<=gridsize)
+   while(i<gridsize)
    {
        grid[i][j] = 1;
        std::cout << grid[i][j];
        i++;
    }
+   std::cout << std::endl;
    i=1;
-   while(i<=gridsize)
+   while(i<gridsize)
    {
        j=1;
        while(j<i)
        {
-           grid[i][j] = grid[i-1][j]+grid[i][j-1];
+           grid[i][j] = grid[i-1][j]+grid[i][j-1];  //double check this
            j++;
        }
        grid[i][j] = 2*grid[i-1][j];
        i++;
    }
    
-   std::cout << grid[gridsize-1][gridsize-1] << std::endl;
+   /*Debugging*/
+   for(i=0;j<gridsize;i++)
+   {
+    for(j=0;j<gridsize;j++)
+    {
+        std::cout << grid[i][j];
+    }
+    std::cout << std::endl;
+   }
+
+   //std::cout << grid[gridsize-1][gridsize-1] << std::endl;
    return 0;
 }
